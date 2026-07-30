824c6dde63748f54105018bd8cc66dea2a2cad6f
Dyk <jarabadaniela97@gmail.com>
2026-07-30 16:01:58 -0500

    actividad

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-import express from "express";
-import "dotenv/config";
-import bodyparser from "body-parser";//importacion es "module"//
+import express from 'express';
+import 'dotenv/config';
 
 const app = express();
-const port = process.env.PORT || 3030;
+const puerto = process.env.PORT || 3030;
 
-//configurar el uso de body-parse para nuestra aplicacion
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// Configuración
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-    res.send("Hola, estamos aprendiendo Express con la ficha 3407184");
+export const saludo = 'Hola, estamos aprendiendo Express con la ficha 3407184';
+
+app.get("/", (_, res) => {
+    res.send(saludo);
 });
 
-//otro endpoint, funcion de flecha
-app.get("/productos", (req,res)=>{
-    //usando template string ``
-    res.send(`<h1>listado de productos</h1>
+app.get("/productos", (req, res) => {
+    res.send(`
+        <h1>Lista de productos</h1>
         <ol>
-        <li>tv</li>
-        <li>phone</li>
-        <li>audifonos</li>
-        <ol> `)
-} )
+            <li>Televisor</li>
+            <li>Celular</li>
+            <li>Impresora</li>
+        </ol>
+    `);
+});
 
+app.get("/productos/:name/:id/:precio", (req, res) => {
+    const producto = req.params.name;
+    const id_producto = req.params.id;
+    const precio_producto = req.params.precio;
 
-app.listen(port, function () {
-    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
+    res.send(`
+        <h1>Información del producto</h1>
+        <ol>
+            <li>Producto: ${producto}</li>
+            <li>Id: ${id_producto}</li>
+            <li>Precio: ${precio_producto}</li>
+        </ol>
+    `);
+});
+
+app.get("/saludo/:name", (req, res) => {
+    const nombre = req.params.name;
+    res.send(`Hola ${nombre}`);
+});
+
+app.get("/category/:Category/:id", (req, res) => {
+    const categoria_producto = req.params.Category;
+    const id_categoria = req.params.id;
+
+    res.send(`
+        <h1>Productos por categoría</h1>
+        <ol>
+            <li>Categoría: ${categoria_producto}</li>
+            <li>Id: ${id_categoria}</li>
+        </ol>
+    `);
+});
+
+app.listen(puerto, () => {
+    console.log(`Servidor funcionando en el puerto: ${puerto}`);
 });
