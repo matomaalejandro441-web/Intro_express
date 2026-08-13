2bfeb89627a73f3384419d39173fe789d6c0bf9d
Daniela <jarabadaniela97@gmail.com>
2026-08-13 17:15:45 -0500

    activad de requerimientos  13/08/2026

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3090;
 
+app.use(express.json());
+
 app.get("/", function (req,res) {
     res.send("API de aprendices, Endpoint principal")
 })
@@ -10,11 +12,11 @@ const listaAprendices=[{"nombre":"ana","edad":22,"correo":"juan@gmail.com","imgp
     {"nombre":"james","edad":39,"correo":"jam@gmail.com","imgperfil":"url"}, 
     {"nombre":"pablito","edad":44,"correo":"pablito@gmail.com","imgperfil":"url"}]
 
-app.get("/aprendices", (req,res) => {
+app.get("/api/aprendices", (req,res) => {
     res.json(listaAprendices)
 })
 
-app.get("/aprendices/:nombre", (req, res) => {
+app.get("/api/aprendices/:nombre", (req, res) => {
     const nombreBusqueda = req.params.nombre.toLowerCase();
 
     const aprendizEncontrado = listaAprendices.find(
@@ -24,7 +26,30 @@ app.get("/aprendices/:nombre", (req, res) => {
     res.json(aprendizEncontrado);
 });
 
+app.post('/aprendicescreado', (req, res) => {
+    const { nombre, edad, correo, imgperfil } = req.body;
+    
+    if (typeof nombre !== 'string' || nombre.trim().length < 3) {
+        return res.status(400).json({ "error": "El nombre como mínimo necesita 3 letras" });
+    }
+    
+    if (typeof correo !== 'string' || !correo.includes('@')) {
+        return res.status(400).json({ "error": "El correo necesita @" });
+    }
+    
+    const datosAprendiz = { nombre, edad, correo, imgperfil };
+    listaaprendices.push(datosAprendiz);
+    
+    return res.status(201).json({ "mensaje": "Aprendiz creado", "Datos": datosAprendiz });
+});
+
+app.post ("/api/aprendices2",(req,res) => {
+    const datosAprendiz = req.body
+    listaAprendices.push(datosAprendiz);
+    res.status(201)
+    res.json({"mensaje":"aprendiz creado","datos": datosAprendiz}) ;
+});
 
 app.listen(port, function()  {
-    console.log(`SERVIDOR: http://localhost:${port}`)
+    console.log(`SERVIDOR INICIADO: http://localhost:${port}`)
 })
