4829c41910df7ae3d98d3451cc820934d186b6cd
Daniela <jarabadaniela97@gmail.com>
2026-08-13 15:35:56 -0500

    activad de listado 13/08/2026

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3090;
 
-app.get("/", (_, res) => {
-    res.send("Hola, estamos aprendiendo Express con la ficha 3407184");
+app.get("/", function (req,res) {
+    res.send("API de aprendices, Endpoint principal")
+})
+
+const listaAprendices=[{"nombre":"ana","edad":22,"correo":"juan@gmail.com","imgperfil":"url"},
+    {"nombre":"james","edad":39,"correo":"jam@gmail.com","imgperfil":"url"}, 
+    {"nombre":"pablito","edad":44,"correo":"pablito@gmail.com","imgperfil":"url"}]
+
+app.get("/aprendices", (req,res) => {
+    res.json(listaAprendices)
+})
+
+app.get("/aprendices/:nombre", (req, res) => {
+    const nombreBusqueda = req.params.nombre.toLowerCase();
+
+    const aprendizEncontrado = listaAprendices.find(
+        (aprendiz) => aprendiz.nombre.toLowerCase() === nombreBusqueda
+    );
+
+    res.json(aprendizEncontrado);
 });
 
-app.listen(port, () => {
-    console.log(`Servidor en funcionamiento en el puerto: ${port}`);
-});
+
+app.listen(port, function()  {
+    console.log(`SERVIDOR: http://localhost:${port}`)
+})
